893c7d3d32883cd460b6660de5eaf696a97fb667
Mourning Dove <mourningdove007@gmail.com>
2025-10-07 12:52:56 -0500

    Updating directory structure.

--- exceptions/blockchain_exception.py
+++
@@ -1,7 +0,0 @@
-class BlockchainError(Exception):
-
-    def __init__(self, message: str, *args):
-
-        super().__init__(message, *args)
-
-        self.message = "Blockchain Error: " + message
