32929b2bf558ad20ba078b660aaeebdb58caf185
mourningdove007 <mourningdove007@gmail.com>
2025-10-09 20:40:10 -0500

    additional public value for cow protocol

--- foundry/src/PublicValues.sol
+++ foundry/src/PublicValues.sol
@@ -6,9 +6,13 @@ abstract contract  PublicValues {
     // https://etherscan.io/token/0x6b175474e89094c44da98b954eedeac495271d0f
     address internal constant DAI_TOKEN_ADDRESS = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     
-    //The CoW token contract address can be found on etherscan
+    // The CoW token contract address can be found on etherscan
     // https://etherscan.io/token/0xdef1ca1fb7fbcdc777520aa7f396b4e015f497ab#code
     address internal constant COW_TOKEN_ADDRESS = 0xDEf1CA1fb7FBcDC777520aa7f396b4E015F497aB;
+
+    // The CoW Settlement Contract
+    // https://etherscan.io/address/0x9008D19f58AAbD9eD0D60971565AA8510560ab41
+    address internal constant COW_SETTLEMENT_ADDRESS = 0x9008D19f58AAbD9eD0D60971565AA8510560ab41;
 
     // WETH smart contract can be found on etherscan
     // https://etherscan.io/token/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2
